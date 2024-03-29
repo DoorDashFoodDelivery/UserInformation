eff9339540df34a4a5aa45484e627c059bb81044
dhananjay <waradedhananjay2000@gmail.com>
2024-03-29 15:41:08 +0530

    create add user method in user controller

--- src/main/java/com/example/UserInfo/controller/UserController.java
+++ src/main/java/com/example/UserInfo/controller/UserController.java
@@ -1,5 +1,27 @@
 package com.example.UserInfo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.UserInfo.dto.UserDTO;
+import com.example.UserInfo.service.UserService;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	UserService userService;
+	
+	@PostMapping("/addUser")
+	public ResponseEntity<UserDTO>addUser(@RequestBody UserDTO userDTO ){
+	UserDTO savedUser=userService.addUser(userDTO);
+		return new ResponseEntity<>(savedUser, HttpStatus.OK);
+		
+	}
 }
